893e6445acd29a73ea32bf7b073e1ced779647bd
dh00mk3tu <38985076+dh00mk3tu@users.noreply.github.com>
2019-04-25 23:29:11 +0530

    Add files via upload

---
+++ anya.cpp
@@ -0,0 +1,78 @@
+/*
+
+Created on Fri Jan 11 15:25:17 2019
+
+author: dh00mk3tu
+
+Code Descp 
+
+	This is a code snippet form now obsolete version of AnYA
+	This file is installed to check if the installation is working fine.
+	If this file is present on your computer this means that installaion 
+	was fully/partially successful. 
+	
+	Thank you for contributing in development of AnYa. I will work hard 
+	on this project as long as I can. 
+	Forward Unto Dawn
+	~ Anirudh Rath | dh00mk3tu
+		
+
+Descp
+
+    AnYa is being developed by dh00mk3tu with the aim 
+    to make it a system monitoring AI and to help achieve 
+    our daily tasks faster and precisely. AnYa is currently 
+    in second generation of it's life cycle. The original 
+    was created in C++ and was very limited in terms of 
+    library support. 
+    
+    This is second generation of AnYa initiated in September 
+    2018. AnYa was re written from scratch in Python 3.x so 
+    as to support wide variety of features which were planned 
+    to be implemented using C++
+    
+    AnYa shall remain a terminal based, CLI program for long 
+    and a system embedded version or a web browser based version 
+    is far from seeing light
+    
+
+Version Nomenclature 
+ 
+    Version Nomenclature in AnYa follows the following pattern 
+    
+    Generation.update.times_updated 
+    
+    The generation refers to the current active generation of AnYa
+    The update refers to the version when a new feature was introduced.
+    times_updated refers to the number of times the previous version was 
+    appended with a change. 
+    
+    For Example; AnYa is currently in generation 2. Last feature 
+    that was introduced was eye tracking, numbered 4. Times the 
+    previous feature was update is one. So, the latest version of
+    AnYa is 2.4.1 or simply 2.x
+    
+
+
+Future Updates 
+
+    Here's a list of features which may see the light in the future updates
+    1. Cursor with eyes
+    2. Automated Cursor 
+    3. Automated netstat
+    4. face recog
+    5. Encrypted protection to access AnYa 
+    6. Chat Bot
+    7. File cleanup automated 
+*/
+
+#include <iostream>
+#include <stdlib.h>
+//#include "anyatest.h"
+
+using namespace std;
+
+int main() {
+    cout<<"\n\t\t Installaion Successfull\n";
+    cout<<"\n\t\t Other files will now show here: \n";
+}
